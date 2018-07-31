8e4e64a189ee4acf10b05af9396973d237c642a8
Sandbar <Sandbar>
2018-07-31 13:48:39 +0800

    update file

--- puPure_Audiences.py
+++ puPure_Audiences.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 from pymongo import MongoClient
 import configparser
+import os
+import copy
 
 class GenerateNewPTs:
 
@@ -12,6 +14,7 @@ class GenerateNewPTs:
         self.n_differs = 2
         self.old_pt = dict()
         self.old_pt_dict = dict()
+        self.exist_ads_id = list()
         self.df_com_features = pd.DataFrame()
 
     ''' 读取配置文件中的db数据并将需要连接 '''
@@ -49,23 +52,30 @@ class GenerateNewPTs:
 
     ''' 将获得的pt转化为dict dict[name]=id'''
     def get_value_dict(self):
-        behaviors = self.old_pt['pt']['adset_spec']['targeting']['behaviors']
-        if isinstance(behaviors, list):
-            for index in range(len(behaviors)):
-                behavior = behaviors[index]
-                self.old_pt_dict[behavior['id']] = behavior['name']
-        elif isinstance(behaviors, dict):
-            for k, v in behaviors.items():
-                self.old_pt_dict[v['id']] = v['name']
-
-        interests = self.old_pt['pt']['adset_spec']['targeting']['interests']
-        if isinstance(interests, list):
-            for index in range(len(interests)):
-                interest = interests[index]
-                self.old_pt_dict[interest['id']] = interest['name']
-        elif isinstance(interests, dict):
-            for k, v in interests.items():
-                self.old_pt_dict[v['id']] = v['name']
+        try:
+            if self.old_pt['pt']['adset_spec']['targeting'].get('behaviors'):
+                behaviors = self.old_pt['pt']['adset_spec']['targeting']['behaviors']
+                if isinstance(behaviors, list):
+                    for index in range(len(behaviors)):
+                        behavior = behaviors[index]
+                        self.old_pt_dict[behavior['id']] = behavior['name']
+                elif isinstance(behaviors, dict):
+                    for k, v in behaviors.items():
+                        self.old_pt_dict[v['id']] = v['name']
+        except ValueError:
+            print('behaviors is not exist or ValueError!')
+        try:
+            if self.old_pt['pt']['adset_spec']['targeting'].get('interests'):
+                interests = self.old_pt['pt']['adset_spec']['targeting']['interests']
+                if isinstance(interests, list):
+                    for index in range(len(interests)):
+                        interest = interests[index]
+                        self.old_pt_dict[interest['id']] = interest['name']
+                elif isinstance(interests, dict):
+                    for k, v in interests.items():
+                        self.old_pt_dict[v['id']] = v['name']
+        except ValueError:
+            print('interests is not exist or ValueError!')
 
     ''' 产生新pt的主进入口 '''
     def get_new_pt(self):
@@ -90,38 +100,66 @@ class GenerateNewPTs:
     ''' 比较信息熵（-plogp），然后将待选择的id放入到指定的集合 '''
     def cmp_value(self, pt_tmp, tmp_value):
         tindex = pt_tmp.index(min(pt_tmp))
-        pt_tmp[tindex] = pt_tmp[tindex] - tmp_value * np.log2(tmp_value)
+        pt_tmp[tindex] = pt_tmp[tindex] - (tmp_value+0.0001) * np.log2(tmp_value+0.0001)
         return tindex
 
     ''' 将生成的新的behaviors和interests合并成新的pt '''
     def reverse_generate_new_pt(self, pt_behaviors_ads=None, pt_interests_ads=None):
         tmp_pts = []
         for index in range(self.n_differs):
-            pt_interests = {}
+            new_pt = self.old_pt
             pt_behaviors = {}
             for bindex in range(len(pt_behaviors_ads[index])):
                 bid = str(pt_behaviors_ads[index][bindex])
                 if self.old_pt_dict.get(bid):
                     pt_behaviors[str(bindex)] = {'id': bid, 'name': self.old_pt_dict[bid]}
 
+            pt_interests = {}
             for iindex in range(len(pt_interests_ads[index])):
-                iid = pt_interests_ads[index][iindex]
+                iid = float(pt_interests_ads[index][iindex])
                 if self.old_pt_dict.get(iid):
                     pt_interests[str(iindex)] = {'id': iid, 'name': self.old_pt_dict[iid]}
-            self.old_pt['pt']['adset_spec']['targeting']['behaviors'] = pt_behaviors
-            self.old_pt['pt']['adset_spec']['targeting']['interests'] = pt_interests
-            tmp_pts.append(self.old_pt)
+            if pt_behaviors is not None or pt_interests is not None:
+                new_pt['pt']['adset_spec']['targeting']['behaviors'] = pt_behaviors
+                new_pt['pt']['adset_spec']['targeting']['interests'] = pt_interests
+                # print(pt_behaviors)
+                # print(pt_interests)
+                tmp_pts.append(copy.deepcopy(new_pt))
+                print(tmp_pts)
+                print('-'*300)
         return tmp_pts
 
     ''' 接收一个条件参数进行查询并获取创建时间最近的一个广告的pt并返回 '''
     def find_a_ads(self, condition='bet4_ios_us'):
         db, client = self.mongodb_conn()
-        colles_ads = db.ads.find({'delt_name': condition}, {'_id': 0, 'pt': 1}).sort([('create_time', -1)]).limit(1)
+        exist_ads = self.find_exist_ads()
+        if len(exist_ads) > 0:
+            colles_ads = db.ads.find({'delt_name': condition, 'ad_id': {'$nin': exist_ads}},
+                                     {'_id': 0, 'ad_id': 1, 'pt': 1}).sort([('create_time', -1)]).limit(1)
+        else:
+            colles_ads = db.ads.find({'delt_name': condition},
+                                     {'_id': 0, 'ad_id': 1, 'pt': 1}).sort([('create_time', -1)]).limit(1)
         tmp_pt = dict()
         for item in colles_ads:
             tmp_pt = item
+        exist_ads.append(str(tmp_pt['ad_id']))
+        self.exist_ads_id = exist_ads
         client.close()
         return tmp_pt
+
+    ''' 找到已经使用过的广告并返回广告的id列表 '''
+    def find_exist_ads(self):
+        if os.path.exists('exist_ads_id.txt'):
+            exist_ads = pd.read_csv('exist_ads_id.txt')
+            exist_ads = exist_ads['ad_id'].apply(lambda x: str(x)).tolist()
+            return exist_ads
+        else:
+            return list()
+
+    ''' 将本次使用的广告id加入到使用过的广告列表中进行保存 '''
+    def save_to_exist_ads(self):
+        if len(self.exist_ads_id) > 0:
+            pd.DataFrame({'ad_id': self.exist_ads_id}).to_csv('exist_ads_id.txt', index=False)
 
     ''' 
     修改pt中的名字以标注此广告由本程序编写投放
@@ -130,10 +168,13 @@ class GenerateNewPTs:
     3、pt.adset_spec.campaign_spec.name
      '''
     def add_name_sign(self, old_pt):
-        s = ' [plogp]'
-        old_pt['pt']['name'] = old_pt['pt']['name']+ s
-        old_pt['pt']['adset_spec']['name'] = old_pt['pt']['adset_spec']['name'] + s
-        old_pt['pt']['adset_spec']['campaign_spec']['name'] = old_pt['pt']['adset_spec']['campaign_spec']['name'] + s
+        try:
+            s = ' [plogp]'
+            old_pt['pt']['name'] = old_pt['pt']['name']+ s
+            old_pt['pt']['adset_spec']['name'] = old_pt['pt']['adset_spec']['name'] + s
+            old_pt['pt']['adset_spec']['campaign_spec']['name'] = old_pt['pt']['adset_spec']['campaign_spec']['name'] + s
+        except:
+            pass
         return old_pt
 
     ''' main主进入口'''
@@ -141,9 +182,13 @@ class GenerateNewPTs:
         self.n_differs = n_differs
         self.old_pt = self.add_name_sign(old_pt)
         self.get_value_dict()
-        self.transformate_to_df(value_calc_prob)
-        newpts = self.get_new_pt()
-        return newpts
+        if self.old_pt_dict is None:
+            return None
+        else:
+            self.transformate_to_df(value_calc_prob)
+            newpts = self.get_new_pt()
+            self.save_to_exist_ads()
+            return newpts
 
 
 if __name__ == '__main__':
@@ -152,6 +197,7 @@ if __name__ == '__main__':
     import heapp
     value_prob = heapp.test()
     pts = gnpt.main(pt, value_prob, 3)
+    # print(pts)
     print('local test!!!')
 
 
